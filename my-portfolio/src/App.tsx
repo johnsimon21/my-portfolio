144454ebef89a50df075cd8c8ff2f160d6395a99
johnsimon21 <120222249+johnsimon21@users.noreply.github.com>
2024-11-21 05:15:03 +0100

    Update App.tsx

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -3,11 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <div className="w-full h-screen bg-[#141E46] px-5">
+    <div className="w-full h-screen bg-[#141E46] px-[80px]">
       <header className="border-b-2 border-[#182751] h-[210px] flex">
         <nav className="flex bg-white w-full">
 
-          <div className="justify-self-start grow align-items-center">
+          <div className="justify-self-start grow flex items-center ">
             <h1 className="logo">JoystLine</h1>
           </div>
  
