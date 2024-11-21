0e00cf5b9b237f9fcaa5e1f00a90f8ea0623415a
johnsimon21 <120222249+johnsimon21@users.noreply.github.com>
2024-11-21 06:19:20 +0100

    add style in heade and menu items

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -5,18 +5,22 @@ function App() {
   return (
     <div className="w-full h-screen bg-[#141E46] px-[80px]">
       <header className="border-b-2 border-[#182751] h-[210px] flex">
-        <nav className="flex bg-white w-full">
+        <nav className="flex w-full">
 
-          <div className="justify-self-start grow flex items-center ">
-            <h1 className="logo">JoystLine</h1>
+          <div className="justify-self-start grow flex items-center text-4xl font-semibold ">
+            <h1 className="logo text-[#D0D8EA]">
+              <span className="text-[#208FBB]">{"<"}</span>
+              JoystLine
+              <span className="text-[#208FBB]">{">"}</span>
+            </h1>
           </div>
- 
-          <div className="justify-self-end grow bg-black text-white">
-            <div className="about">About</div>
-            <div className="skills">Skills</div>
-            <div className="projects">Projects</div>
-            <div className="contacts">Contacts</div>
-          </div>
+
+          <ul className="menu flex grow items-center text-white">
+            <li className="about me-[15px] bg-[#0E1F52] py-[15px] px-[30px] rounded-full cursor-pointer text-[#99B2EC] menu-item-active">About</li>
+            <li className="skills mx-[15px] bg-[#0E1F52] py-[15px] px-[30px] rounded-full cursor-pointer text-[#99B2EC]">Skills</li>
+            <li className="projects mx-[15px] bg-[#0E1F52] py-[15px] px-[30px] rounded-full cursor-pointer text-[#99B2EC]">Projects</li>
+            <li className="contacts mx-[15px] bg-[#0E1F52] py-[15px] px-[30px] rounded-full cursor-pointer text-[#99B2EC]">Contacts</li>
+          </ul>
         </nav>
       </header>
     </div>
